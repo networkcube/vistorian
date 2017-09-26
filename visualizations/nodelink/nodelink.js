a0dbcf542a429a5c34ded468bb57df90301c8ff0
FEKETE Jean-Daniel <jean-daniel.fekete@inria.fr>
2017-09-26 23:12:41 +0200

    fix http/https and port number

--- visualizations/nodelink/nodelink.js
+++ visualizations/nodelink/nodelink.js
@@ -416,7 +416,8 @@ function calculateCurvedLinks() {
                 { x: multiLink.source.x, y: multiLink.source.y },
                 { x: multiLink.source.x, y: multiLink.source.y },
                 { x: multiLink.target.x, y: multiLink.target.y },
-                { x: multiLink.target.x, y: multiLink.target.y }];
+                { x: multiLink.target.x, y: multiLink.target.y }
+            ];
         }
         else {
             links = multiLink.links().toArray();
@@ -435,7 +436,8 @@ function calculateCurvedLinks() {
             else {
                 dir = {
                     x: multiLink.target.x - multiLink.source.x,
-                    y: multiLink.target.y - multiLink.source.y };
+                    y: multiLink.target.y - multiLink.source.y
+                };
                 offset = stretchVector([-dir.y, dir.x], LINK_GAP);
                 offset2 = stretchVector([dir.x, dir.y], LINK_GAP);
                 for (var j = 0; j < links.length; j++) {
@@ -445,7 +447,8 @@ function calculateCurvedLinks() {
                             y: (multiLink.source.y + offset2[1] + (j - links.length / 2 + .5) * offset[1]) },
                         { x: multiLink.target.x - offset2[0] + (j - links.length / 2 + .5) * offset[0],
                             y: (multiLink.target.y - offset2[1] + (j - links.length / 2 + .5) * offset[1]) },
-                        { x: multiLink.target.x, y: multiLink.target.y }];
+                        { x: multiLink.target.x, y: multiLink.target.y }
+                    ];
                 }
             }
         }
